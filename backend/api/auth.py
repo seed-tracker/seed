fbada85de18440bf5575f8750a2bf301782e6a2c
vik-wed <wedel.viktoria@gmail.com>
2023-03-17 18:07:22 -0400

    Continue writing auth

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,22 +1,19 @@
-from flask import Flask
-import db from db
+from flask import Flask, request
 from app import app
+from db import db
 import bcrypt
-
-@app.route('/auth')
-def signup():
-  return "Signup"
 
 @app.route('/auth/login', methods=['POST'])
 def login():
     email = request.form.get('email')
+    print(email)
     password = request.form.get('password')
-    user = db.users.find_one_or_404({"email": email, "password": password})
+    user = db.users.find_one({email: email, password: password})
     # password_checked = bcrypt.check_password_hash(password, user.password)
     # if password_checked == true:
     #     login_user(user)
         # return redirect('/home')
-    return "confirmed"
+    return user
     # else:
     #     print('MPASS_WORG')
 
