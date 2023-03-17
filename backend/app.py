8bf434e3a0433b586f15a906821a55d45018acf5
vik-wed <wedel.viktoria@gmail.com>
2023-03-17 16:28:32 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,14 @@
 from flask import Flask
-from api.auth import *
 from dotenv import load_dotenv, find_dotenv
 import os
 load_dotenv(find_dotenv())
 # from routes import routes_bp
 from flask_cors import CORS
-cors = CORS(app)
-
 app = Flask(__name__)
 
+from api.auth import *
+from api.symptoms import *
+cors = CORS(app)
 # to protect the app
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
