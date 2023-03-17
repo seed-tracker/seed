5895924aade91424f9e03d8cd986acb40d46077f
judazzle <scujkuo@gmail.com>
2023-03-17 16:17:15 -0500

    get all foods and get single food route

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
+from flask_cors import CORS
+# from routes import routes_bp
+from flask import Flask
+# from backend.api import *
+import pprint
+import os
+
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
-import os
-import pprint
 
-from flask import Flask
 # from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -11,11 +15,12 @@ app = Flask(__name__)
 # to protect the app
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-from routes import routes_bp
-from flask_cors import CORS
+from api.foodroutes import *
+
 cors = CORS(app)
 
-app.register_blueprint(routes_bp)
+# app.register_blueprint(routes_bp)
+
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
