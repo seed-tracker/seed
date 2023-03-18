f2143692535d7a47761064b5396f474af7cf1c5c
Wanyi Ng <wanyi.ng1975@gmail.com>
2023-03-17 23:30:22 -0400

    write single symptoms route and add comments

--- backend/api/symptoms.py
+++ backend/api/symptoms.py
@@ -2,10 +2,22 @@ from flask import Flask
 from app import app
 from db import db
 
-@app.route("/symptoms", methods=["GET"])
+# serves all symptoms
+@app.route("/symptoms/", methods=["GET"])
 def get_symptoms():
     symptoms = db.symptoms.find()
-    symptomsList = [{key: str(symptom[key]) for key in symptom} for symptom in symptoms]
-    return {
-        "data": symptomsList
-    }, 200
+    if symptoms:
+        symptomsList = [{key: str(symptom[key]) for key in symptom} for symptom in symptoms]
+        return {"data": symptomsList}, 200
+    else:
+        return "Symptoms not found", 404
+
+@app.route('/symptoms/<string:name>', methods=['GET'])
+def get_symptom_by_name(name):
+    # right now only serves single symptom with capital. Ex: /symptoms/Fatigue
+    symptom = db.symptoms.find_one({'name': name})
+    if symptom:
+        symptom['_id'] = str(symptom['_id'])
+        return {"data": symptom}, 200
+    else:
+        return "Symptom not found", 404
