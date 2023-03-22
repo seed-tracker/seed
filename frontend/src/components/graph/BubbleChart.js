e9c9fb9f4e634443ae4bd861f60e7dc806ef51b4
Wanyi Ng <wanyi.ng1975@gmail.com>
2023-03-21 23:46:30 -0400

    import d3

--- frontend/src/components/graph/BubbleChart.js
+++ frontend/src/components/graph/BubbleChart.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectAuthUser } from "../../store/authSlice";
 import { fetchUserCorrelations } from "../../store/correlationsSlice";
+import * as d3 from "d3";
 
 const BubbleChart = () => {
   const dispatch = useDispatch()
@@ -9,8 +10,6 @@ const BubbleChart = () => {
 
   useEffect(() => {
     dispatch(fetchUserCorrelations(user.username))
-    console.log("CORRELATIONS", dispatch(fetchUserCorrelations(user.username))
-)
   })
 
   return <div>Bubble Chart</div>
