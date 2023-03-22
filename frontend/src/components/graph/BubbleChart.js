dd6f8bd8deca9edf20dd9e550e4ced93207e12b1
Jo Lee <jolee224@gmail.com>
2023-03-22 15:07:29 -0400

    function not working

--- frontend/src/components/graph/BubbleChart.js
+++ frontend/src/components/graph/BubbleChart.js
@@ -1,20 +1,39 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectAuthUser } from "../../store/authSlice";
-import { fetchUserCorrelations } from "../../store/correlationsSlice";
+// import { selectAuthUser } from "../../store/authSlice";
+import { fetchUserCorrelations, selectUserCorrelations } from "../../store/correlationsSlice";
 import * as d3 from "d3";
+
+
 
 const BubbleChart = () => {
   const dispatch = useDispatch()
-  const user = useSelector(selectAuthUser)
+  // const user = useSelector(selectAuthUser)
+  const data = useSelector(selectUserCorrelations);
+  console.log("DATA",data)
 
   useEffect(() => {
-    dispatch(fetchUserCorrelations(user.username))
-
-    const graphSection = d3.select("svg")
+    const keys = getUKeys(data);
+    console.log("KEYS",keys);
+    dispatch(fetchUserCorrelations())
+    const graphSection = d3.select("#graph svg")
     graphSection.append("g")
+    // console.log(user.username)
+  }, [data,dispatch])
 
-  }, [dispatch])
+  function getUKeys(data) {
+    let keys = [];
+    data.forEach(obj => {
+      Object.keys(obj).forEach(key => {
+        if (!keys.includes(key)) {
+          keys.push(key);
+        }
+      });
+    });
+    console.log(keys)
+    return keys;
+  }
+
 
 
   return <div id="graph">
