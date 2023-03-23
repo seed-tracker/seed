35538b35d6380a108fc9ae5b7ea5d6bf0697eb69
Jo Lee <jolee224@gmail.com>
2023-03-22 22:39:57 -0400

    working on food group colors

--- frontend/src/components/graph/BubbleChart.js
+++ frontend/src/components/graph/BubbleChart.js
@@ -3,7 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 // import { selectAuthUser } from "../../store/authSlice";
 import { fetchUserCorrelations, selectUserCorrelations } from "../../store/correlationsSlice";
 import * as d3 from "d3";
+let categoryColorScale;
 
+const foodGroups=["Goat", "Fish", "Processed Foods", "Beans,Peas,and Soy","Fruit", "Pork", "Beef", "Milk,Yogurt,and Cheese",
+"Nuts and Seeds", "Vegetables, Starchy", "Grains, Gluten-Free", "Shellfish", "Lamb", "Refined Sugars", "Vegetables, Non-Starchy",
+"Eggs", "Caffeinated Beverages", "Grains, Gluten", "Other  Seafoods", "Poultry"]
 const BubbleChart = () => {
   const dispatch = useDispatch()
   // const user = useSelector(selectAuthUser)
@@ -36,10 +40,10 @@ const symptom = datas.map((obj) => obj.symptom);
 const count = datas.map((obj) => obj.count);
 
 const top_foods = datas.flatMap((obj) => obj.top_foods);
-console.log(top_foods)
+
 
 const top_groups = datas.flatMap((obj) => obj.top_groups);
-
+console.log(top_groups)
   useEffect(() => {
     dispatch(fetchUserCorrelations())
     // const graphSection = d3.select("#graph svg")
@@ -57,25 +61,28 @@ const top_groups = datas.flatMap((obj) => obj.top_groups);
   // const countScale = d3.scaleLinear()
   // .domain([0, d3.max(data, d => d.count)])
   // .range([10, 50]);
+ 
   const circles = svg.selectAll("circle")
   .data(top_foods)
   .join("circle")
   .attr("cx", (d, i) => i * 100 + 50)
   .attr("cy", 50)
   .attr("r", 20)
-  .attr("fill", (d)=>d.lift>1.01 ? "red":"green")
+  // .attr("fill", (d)=>d.lift>1.01 ? "red":"green")
+  .attr('fill', d => categoryColorScale(d.top_groups))
   .attr("stroke", "white")
   .attr("stroke-width", 2);
   })
 
-
+  const colors = ['#ffcc00', '#ff6666', '#cc0066', '#66cccc', '#f688bb', '#65587f', '#baf1a1', '#333333', '#75b79e',
+    '#66cccc', '#9de3d0', '#f1935c', '#0c7b93', '#eab0d9', '#baf1a1', '#9399ff','#06AED5', '#086788', '#F0C808', '#DD1C1A']
 //select,data,join is combo rather than enter, append b/c if you use enter you have to manually update when data changes
 //select,data,join auto updates when data changes
 //datum is d
 //each mark/variable we want to visualize there should a 1:1 to an attribute with an arrow function
   // const svg = d3.create("svg").attr("width", 932).attr("height", 932).style("display", "block").style("margin", "0 auto").style("background", "cornflowerblue").style("cursor", "pointer")
   // graphSection.append("svg")
-
+  categoryColorScale = d3.scaleOrdinal(foodGroups, colors)
   const svg = d3.select("svg")
   svg.append("g")
 
@@ -87,3 +94,8 @@ const top_groups = datas.flatMap((obj) => obj.top_groups);
 }
 
 export default BubbleChart
+
+//food group = color
+//lift = size
+//severity = gradient
+//food = bubble
