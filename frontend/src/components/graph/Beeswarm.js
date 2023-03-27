9080e7086940caf362394310c47c14378406b415
Jo Lee <jolee224@gmail.com>
2023-03-26 21:43:45 -0400

    x axis

--- frontend/src/components/graph/Beeswarm.js
+++ frontend/src/components/graph/Beeswarm.js
@@ -1,16 +1,23 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getUserStats, selectUserCorrelations } from "../../store/correlationsSlice";
-
+import { select } from "d3-selection";
+import { scaleLinear } from "d3-scale";
+import { axisBottom } from "d3-axis";
+import correlationsSlice from "../../store/correlationsSlice";
+import { selectUserCorrelations } from "../../store/correlationsSlice";
+import { getUserStats } from "../../store/correlationsSlice";
+import * as d3 from "d3";
 
 const Beeswarm = () => {
+  const svgRef = useRef();
+
   const dispatch = useDispatch()
   const data = useSelector(selectUserCorrelations)
   // console.log("DATA", data)
 
   const handleGetAllTime = async (all) => {
     await dispatch(getUserStats("all"))
-    // console.log("All", dispatch(getUserStats("all")))
+    console.log("All", dispatch(getUserStats("all")))
   }
   const handleGetSixMonths = async (halfYear) => {
     await dispatch(getUserStats(180))
@@ -21,6 +28,23 @@ const Beeswarm = () => {
     // console.log("year", dispatch(getUserStats(365)))
   }
 
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    const margin = {top: 250, right: 10, bottom: 20, left: 10};
+    const width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom;
+      
+svg.selectAll("svg")
+    const xScale = scaleLinear()
+      .domain([0, 1000]) 
+      .range([0, 500]); 
+    const xAxis = axisBottom(xScale);
+
+    svg.append("g")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .call(xAxis);
+  }, []);
 
   return (
     <section>
@@ -31,13 +55,9 @@ const Beeswarm = () => {
       <button type="button" onClick={handleGetAllTime} value="all">All</button>
       <button type="button" onClick={handleGetSixMonths} value="180">6 Months</button>
       <button type="button" onClick={handleGetOneYear} value="365">1 Year</button>
-      {/* <span>Toggle Food Groups:</span>
-      <label> */}
-        {/* map values length of arr */}
-        {/* <input type="checkbox" />
-      </label> */}
+     <div className="beeSwarmChart"><svg ref={svgRef} width="700" height="300"></svg></div> 
     </section>
-  )
-}
+  );
+};
 
-export default Beeswarm
+export default Beeswarm;
