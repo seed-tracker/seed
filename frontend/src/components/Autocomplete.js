b4da783c6b5c501120588d5e36eca014aca8f59a
judazzle <scujkuo@gmail.com>
2023-03-27 09:10:31 -0500

    Navbar styled with NextUI

--- frontend/src/components/Autocomplete.js
+++ frontend/src/components/Autocomplete.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {useDispatch} from 'react-redux';
+// import {useDispatch} from 'react-redux';
 // import { autocompleteFood } from "../store/entrySlice";
 import apiClient from "../config";
 
