09a1d4d9dc6357b778074caa2f6fc235a5f2a3c9
vik-wed <wedel.viktoria@gmail.com>
2023-03-27 10:29:41 -0400

    Update footer content

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,27 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p>Footer info here</p>
+      <h3>Our Mission</h3>
+      <p>
+        Finding correlations between symptoms and foods, allowing you to narrow
+        down food-based triggers. <br />
+        <strong>
+          Please remember that correlation DOES NOT equal causation and SEED
+          does not provide medical advice!
+        </strong>
+      </p>
+      <h3>Medical Advice Disclaimer</h3>
+      <p>
+        This web site is provided for informational purposes only and does not
+        constitute providing medical advice or professional services. The
+        information provided should not be used for diagnosing or treating a
+        health problem or disease, and those seeking personal medical advice
+        should consult with a licensed physician. <br />
+        <strong>
+          If you think you may have a medical emergency, call 911 or go to the
+          nearest emergency room immediately.
+        </strong>
+      </p>
     </footer>
   );
 };
