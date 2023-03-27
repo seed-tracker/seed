fd9e3c747502b11fb9236bc6ce91d803da52e184
Wanyi Ng <wanyi.ng1975@gmail.com>
2023-03-27 13:06:11 -0400

    line up circles to graph and group circles into g element

--- frontend/src/components/graph/Beeswarm.js
+++ frontend/src/components/graph/Beeswarm.js
@@ -3,7 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { select } from "d3-selection";
 import { scaleBand, scaleLinear } from "d3-scale";
 import { axisBottom, axisLeft } from "d3-axis";
-import { statsSlice, selectUserStats, getUserStats } from "../../store/statsSlice";import * as d3 from "d3";
+import { statsSlice, selectUserStats, getUserStats } from "../../store/statsSlice";
+import * as d3 from "d3";
 
 const Beeswarm = () => {
   const svgRef = useRef();
@@ -34,66 +35,51 @@ const Beeswarm = () => {
 
   useEffect(() => {
     const svg = select(svgRef.current);
-    const margin = {top: 200, right: 10, bottom: 200, left: 50};
-    const width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom;
-
-    // svg.selectAll("svg")
+    const margin = { top: 200, right: 10, bottom: 200, left: 50 };
+    const width = 500 - margin.left - margin.right;
+    const height = 300 - margin.top - margin.bottom;
 
     if (symptoms && symptoms.length > 0) {
       const xScale = scaleBand()
-    // .domain(data.symptoms)
         .domain(symptoms.map((symptom) => symptom.name))
         .range([0, width]);
-      const yScale = scaleLinear()
 
-      .domain([Math.max(...counts)*2])
-      .range([height, 0]);
+      const yScale = scaleLinear()
+        .domain([Math.max(...counts) * 2, 0])
+        .range([height, 0]);
 
       const xAxis = axisBottom(xScale);
       const yAxis = axisLeft(yScale).ticks(5);
 
       const xAxisLine = svg
-      .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top * 2})`)
-      .call(xAxis);
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top * 2})`)
+        .call(xAxis);
 
-    const yAxisLine = svg
-      .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top * 2})`)
-      .call(yAxis)
+      const yAxisLine = svg
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top * 2})`)
+        .call(yAxis);
 
-        const nodes = svg.selectAll("circle")
-        .data(symptoms)
-        .join(
-          enter => enter.append("circle")
-            .attr("cx", d => xScale(d.name))
-            .attr("cy", d => yScale(d.count))
-            .attr("r", 5)
-        );
+        const g = svg.append("g").attr("transform", `translate(${margin.left * 2 - 6}, ${margin.top * 2})`);
 
-        const simulation = d3.forceSimulation()
-        .force("x", d3.forceX().strength(.1).x(d=>xScale(d.name)))
-        .force("y", d3.forceY().strength(.8).y(d=>yScale(d.count)))
-        .force("collide",d3.forceCollide().radius(5));
-
-        simulation.nodes(nodes.nodes())
-      .on("tick", () => {
-        nodes
-          .attr("cx", d => d.x)
-          .attr("cy", d => d.y)
-      })
+        const nodes = g
+          .selectAll("circle")
+          .data(symptoms)
+          .join("circle")
+          .attr("cx", d => xScale(d.name))
+          .attr("r", 2);
 
         for (let i = 0; i < symptoms.length; i++) {
           for (let j = 0; j < counts[i]; j++) {
-
-            svg
-              .append("circle")
+            g.append("circle")
               .attr("cx", xScale(symptoms[i].name))
               .attr("cy", yScale(j))
-              .attr("r", 5);
-
+              .attr("r", 2);
           }
         }
+
+      console.log("nodes", nodes);
     }
   }, [data]);
 
