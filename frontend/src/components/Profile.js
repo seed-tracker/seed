c075518de1071a12addda5b81cf0a5e0bef1b0ab
judazzle <scujkuo@gmail.com>
2023-03-27 15:57:37 -0500

    Created NextUI Template .JS files. They are blank

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -28,7 +28,6 @@ const Profile = () => {
     <main>
       <Sidebar />
       <section>
-        <BubbleChart />
         <ScatterPlot />
         <StatsAndFacts />
       </section>
---
+++ frontend/src/components/nextUI/Slider.js
@@ -0,0 +1,5 @@
+// NOT NextUI: Just the reg component template for sliders
+
+/* Symptom component for severity
+Leah's graph for month
+*/
