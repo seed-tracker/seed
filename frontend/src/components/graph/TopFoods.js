aa58d931b2468c05b7abc7b45bb27da2cf553a1f
Wanyi Ng <wanyi.ng1975@gmail.com>
2023-03-29 11:18:02 -0400

    add color palette for food groups but legend sizing needs rework

--- frontend/src/components/graph/TopFoods.js
+++ frontend/src/components/graph/TopFoods.js
@@ -8,6 +8,7 @@ import {
   selectUserStats,
   getUserStats,
 } from "../../store/statsSlice";
+import { foodGroupsSlice, fetchAllFoodGroups, selectFoodGroups } from "../../store/foodGroupsSlice";
 import * as d3 from "d3";
 import { HeaderText } from "../nextUI/";
 import { Container, Text, Button, Row } from "@nextui-org/react";
@@ -22,11 +23,13 @@ const TopFoods = () => {
   const svgRef = useRef();
   const dispatch = useDispatch();
   const data = useSelector(selectUserStats);
+  const foodGroups = useSelector(selectFoodGroups);
+  const groupNames = foodGroups.data ? foodGroups.data.map((group) => group.name) : [];
   const topFoods = data.foods ? data.foods.slice(0, 10) : [];
   const counts = topFoods ? topFoods.map((food) => food.count) : [];
-
   useEffect(() => {
     dispatch(getUserStats("all"));
+    dispatch(fetchAllFoodGroups());
   }, [dispatch]);
 
   const handleGetAllTime = async (all) => {
@@ -46,10 +49,11 @@ const TopFoods = () => {
     const width = 950 - margin.left - margin.right;
     const height = 500 - margin.top - margin.bottom;
 
+    const g = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
     if (topFoods && topFoods.length > 0) {
-      const g = svg
-        .append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
       // x axis label
       g.append("text")
@@ -80,7 +84,7 @@ const TopFoods = () => {
       const yAxis = axisLeft(yScale).ticks(5);
 
       g.append("g")
-        .attr("transform", `translate(0, ${height})`)
+        .attr("transform", `translate(-1, ${height})`)
         .call(xAxis)
         .selectAll("text")
         .attr("transform", "rotate(-45)")
@@ -90,10 +94,10 @@ const TopFoods = () => {
 
       g.append("g").call(yAxis);
 
-      const colorPalette = d3.schemeSet3; // Define a color palette for the symptoms and map each symptom to a unique color
+      const colorPalette = ["#f44336", "#e81e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4", "#009688", "#4caf50", "#8bc34a", "#cddc39", "#ffeb3b", "#ffc107", "#ff9800", "#ff5722", "#795548", "#9e9e9e", "#607d8b", "#6d28f1", "#064e5f"]; // Define a color palette for the foods and map each food's group to a unique color
       const foodsColors = {};
-      for (let i = 0; i < topFoods.length; i++) {
-        const foodName = topFoods[i].name;
+      for (let i = 0; i < groupNames.length; i++) {
+        const foodName = groupNames[i];
         const colorIndex = i % colorPalette.length;
         foodsColors[foodName] = colorPalette[colorIndex];
       }
@@ -101,16 +105,18 @@ const TopFoods = () => {
       g.selectAll("circle").data(topFoods).join("circle");
 
       for (let i = 0; i < topFoods.length; i++) {
+        const currentFood = topFoods[i];
+        const groupName = currentFood.groups[0];
         for (let j = 0; j < counts[i]; j++) {
-          g.append("circle")
-            .attr("cx", xScale(topFoods[i].name) + margin.left / 2 - 8)
-            .attr("cy", -yScale(j) + height)
-            .attr("fill", foodsColors[topFoods[i].name])
-            .attr("r", j === counts[i] - 1 ? 8 : 2); // larger radius for last element
+           g.append("circle")
+              .attr("cx", xScale(currentFood.name) + (margin.left / 2) - margin.right)
+              .attr("cy", -yScale(j) + height)
+              .attr("fill", foodsColors[groupName])
+              .attr("r", j === counts[i] - 1 ? 8 : 2); // larger radius for last element
           if (j === counts[i] - 1) {
             g.append("text")
-              .attr("x", xScale(topFoods[i].name) + margin.left / 2)
-              .attr("y", height - yScale(j) - 20)
+              .attr("x", xScale(currentFood.name) + (margin.left / 2) - margin.right)
+              .attr("y", -yScale(j) + height - 20)
               .attr("text-anchor", "middle")
               .text("Count: " + counts[i]);
           }
@@ -119,8 +125,8 @@ const TopFoods = () => {
 
       const legend = d3.select("#legend-top-foods"); // Select the svg legend
       const legendMargin = { top: 10, right: 10, bottom: 10, left: 10 };
-      const legendWidth = 950 - margin.left - margin.right;
-      const legendHeight = 300 - margin.top - margin.bottom;
+      const legendWidth = 950 - legendMargin.left - legendMargin.right;
+      const legendHeight = 300 - legendMargin.top - legendMargin.bottom;
 
       legend
         .append("g")
@@ -129,7 +135,7 @@ const TopFoods = () => {
         .join("g")
         .attr(
           "transform",
-          (d, i) => `translate(${i * 80 + 150}, ${legendHeight / 4})`
+          (d, i) => `translate(${i * 80 + legendMargin.left + legendMargin.right}, ${legendHeight / 4})`
         )
         .call((g) => {
           g.append("circle") // Append circle element to each group
@@ -154,7 +160,7 @@ const TopFoods = () => {
     <Container css={{ margin: "5rem 0" }}>
       <HeaderText text="Your Top 10 Foods:" />
       <Text h3>Legend:</Text>
-      <svg id="legend-top-foods" width="950" height="220"></svg>
+      <svg id="legend-top-foods" width="2000" height="220"></svg>
       <svg ref={svgRef} width="950" height="580"></svg>
       <Row>
         <Button.Group color="primary" bordered ghost>
