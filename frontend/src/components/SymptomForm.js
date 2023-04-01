30b4c4ee9c02b9d9f2c838a63b8d35da4f5c0c2c
judazzle <scujkuo@gmail.com>
2023-03-31 23:07:41 -0500

    Updated card & table templates to have the same opaque background as Login Card. Updated MealForm.js to have Time auto set to current time upon load

--- frontend/src/components/SymptomForm.js
+++ frontend/src/components/SymptomForm.js
@@ -176,7 +176,7 @@ const SymptomForm = () => {
               css={{ padding: "1rem" }}
               description="Recent symptoms table"
               headers={[
-                { key: "name", label: "Your recent symptoms" },
+                { key: "name", label: "YOUR RECENT SYMPTOMS" },
                 { key: "button", label: "" },
               ]}
               rows={recentSymptoms}
